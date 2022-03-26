d919aeaaa1ace20845a3cba1c75e4cabb21f168c
Alex Yeary <mercy719@gmail.com>
2022-03-26 14:24:52 -0600

    update basic emotion progress; bldng complex emots

--- Software/simuPerson.py
+++ Software/simuPerson.py
@@ -18,16 +18,94 @@ class SimuPerson:
         emotions to build upon others
         For now, the floats are simple, unfounded estimates that will certainly
         change through trial, error, and research
+
+        Switching to a simpler emotion wheel for now:
+        https://www.interaction-design.org/literature/article/putting-some-emotion-into-your-design-plutchik-s-wheel-of-emotions
         """
-        self.joy = 0
-        self.trust = 0
-        self.fear = 0
-        self.surprise = 0
-        self.sadness = 0
-        self.disgust = 0
-        self.anger = 0
-        self.anticipation = 0
-        if self.joy >= 1 and self.trust >= 1:
+        self.serenity = 0
+        self.acceptance = 0
+        self.interest = 0
+        self.distracted = 0
+        self.pensive = 0
+        self.bored = 0
+        self.annoyed = 0
+        self.apprehensive = 0
+
+        # Basic emotion increases
+        if self.serenity == 3:
+            self.serenity = self.joy = 3
+            self.stress -= 1
+        if self.joy == 7:
+            self.joy = self.ecstasy = 7 # this poses a question: how long can this state last?
+            self.stress -= 4
+        
+        if self.acceptance == 3:
+            self.acceptance = self.trust = 3
+            self.stress -= 5
+        if self.trust == 7:
+            self.trust = self.admire = 7
+            self.stress += 1
+        
+        if self.apprehensive == 3:
+            self.apprehensive = self.fear = 3
+            self.stress += 3
+        if self.fear == 7:
+            self.fear = self.terrified = 7
+            self.stress += 7
+        
+        if self.distracted == 3:
+            self.distracted = self.surprised = 3
+        if self.surprised % 2 == 2:
+            self.stress += 2
+        if self.surprised == 7:
+            self.surprised = self.amazed = 7
+            self.stress -= 2
+        
+        if self.pensive == 3:
+            self.pensive = self.sad = 3
+            self.stress += 1
+        if self.sad == 9:
+            self.sad = self.grief = 10
+            self.stress += 3 * (self.stress // 2)
+        
+        if self.bored == 3:
+            self.bored = self.disgusted = 3
+        if self.disgusted == 7:
+            self.disgusted = self.loathing = 7
+            self.stress += 3
+        
+        if self.annoyed == 3:
+            self.annoyed = self.anger = 3
+            self.stress += 5
+        if self.anger == 5:
+            self.stress += 10
+        if self.anger == 7:
+            self.anger = self.rage = 7
+            self.stress += 2 * (self.stress // 2)
+        
+        if self.interest == 3:
+            self.interest = self.anticipation = 3
+            self.stress -= 1
+        if self.anticipation == 7:
+            self.anticipation = self.vigilance = 7
+            self.stress += 1
+
+        # Serenity + Acceptance = Love
+        if self.serenity and self.acceptance == 1:
             self.love = 1
-        if self.joy >= 1 and self.trust >= 1:
-            self.aroused = 1
+        if self.serenity and self.acceptance == 3:
+            self.joy = 4
+            self.trust = 4
+            self.love = 4
+        if self.joy and self.trust == 7:
+            self.love = 9
+            self.ecstasy = 9
+            self.admire = 9
+        
+        # Acceptance + Apprehensive = Submission
+        if self.acceptance and self.apprehensive == 1:
+            self.submission = 1
+        if self.trust and self.fear == 3:
+            self.submission = 4
+            self.trust = 4
+            self.fear = 4
